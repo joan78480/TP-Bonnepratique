bf47a1a91a510b29b54d187298e3c77e22a5fd35
Antonin M. <antonin@frixel.fr>
2025-04-28 14:03:10 +0200

    Corrige les erreurs credo

--- lib/tp_bonne_pratique/accounts/user.ex
+++ lib/tp_bonne_pratique/accounts/user.ex
@@ -1,10 +1,14 @@
 defmodule TpBonnePratique.Accounts.User do
+  @moduledoc """
+    Schéma définissant un User et les champs qui lui sont associés.
+    User(:address, :height)
+  """
   use Ecto.Schema
   import Ecto.Changeset
 
   schema "users" do
-    field :address, :string
-    field :height, :integer
+    field(:address, :string)
+    field(:height, :integer)
 
     timestamps(type: :utc_datetime)
   end

a1c735ab4e2c793dbe8af71eef9d4a3fc3a35f8d
Antonin M. <antonin@frixel.fr>
2025-04-29 10:30:33 +0200

    Test d'execution du script par pytest

---
+++ regex_verif.py
@@ -0,0 +1,20 @@
+import re
+
+keywords = [
+    "feat",
+    "fix",
+    "docs",
+    "ops",
+    "test",
+    "refactor",
+    "perf",
+    "style",
+    "upgrade",
+    "scaffold",
+]
+keywords_pattern = "|".join(keywords)
+pattern = rf"^[A-Z]+-\d+\s(?:{keywords_pattern})\([^\)]+\).*$"
+
+
+def validate_string(s):
+    return bool(re.match(pattern, s))
